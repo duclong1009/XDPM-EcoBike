ad8f1fbfcfd3e02d04e97bdf16ff264568c5f8a4
dnkhanh45 <66416653+dnkhanh45@users.noreply.github.com>
2022-01-08 21:53:38 +0700

    add test class

---
+++ src/test/java/controller/ValidateSecretCode.java
@@ -0,0 +1,34 @@
+package controller;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+class ValidateExpirationDate {
+
+    private DepositController depositController;
+    @BeforeEach
+    void setUp() throws Exception {
+        depositController = new DepositController();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            ", false",
+            "'', false",
+            "'12', false",
+            "'123451234561234512345', false",
+            "'123a bscd df', false",
+            "'1231asbg, true"
+    })
+
+
+    void test(String s, boolean expected) {
+        // when
+        boolean isValid = depositController.validateSecurityCode(s);
+        //then
+        assertEquals(expected, isValid);
+    }
+
+}
