1b57b2e868353942845ba472a0c3a5ef5f128fea
duclong1009 <duclong10092000@gmail.com.com>
2022-01-08 18:37:36 +0700

    add css

--- src/main/java/subsystem/interbanksystem/TestAPI.java
+++ src/main/java/subsystem/interbanksystem/TestAPI.java
@@ -12,12 +12,12 @@ public class TestAPI {
         InterbankInterface interbank = new InterbankSubsystem();
             CreditCard creditCard = new CreditCard("kstn_group5_2021", "Group 5", "648", "1125");
 
-        PaymentTransaction paymentTransaction = interbank.refund(creditCard, 1000000, "hoan tien 100k");
+        PaymentTransaction paymentTransaction = interbank.refund(creditCard, 10000000, "hoan tien 100k");
         System.out.println(paymentTransaction);
         System.out.println("Refund OK");
 
-        PaymentTransaction paymentTransaction1 = interbank.payRental(creditCard, 500000, "thanh toan 500k");
-        System.out.println(paymentTransaction1);
+//        PaymentTransaction paymentTransaction1 = interbankpayRental(creditCard, 500000, "thanh toan 500k");
+//        System.out.println(paymentTransaction1);
         System.out.println("Pay OK");
     }
 }
